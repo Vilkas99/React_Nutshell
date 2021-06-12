b881b8f2fc8b4a8c670d45f9a890ce38ed07e27f
vilkas99 <victorium99@hotmail.com>
2021-06-12 15:29:45 -0500

    first commit

---
+++ src/components/Mascota.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Mascota = (props) => {
+  return (
+    <div>
+      <div>Hola</div>
+      <h1>{props.nombre}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.raza}</h2>
+    </div>
+  );
+};
+
+export default Mascota;
